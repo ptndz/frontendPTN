defe10d8c6e8d1285f052dabe620bccf0ed83c70
Pham Thanh Nam <ptndev18@gmail.com>
2023-04-23 15:18:58 +0700

    xx

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -133,7 +133,7 @@ const Login = () => {
                     <button
                       disabled
                       type="button"
-                      className="w-full flex justify-center py-2 px-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 items-center">
+                      className="w-full flex justify-center py-2 px-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 items-center">
                       <svg
                         role="status"
                         className="inline mr-3 w-4 h-4 text-white animate-spin"
